e93ca4d1bf39d87a4683b9aa22565c12db1f79cd
jainor <jainorcardenas@gmail.com>
2022-01-16 23:20:32 -0500

    chore: update files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      context: ({ req }) => ({ req }),
+    }),
+    CategoriesModule,
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
