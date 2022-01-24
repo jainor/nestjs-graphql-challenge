ee157f1fc6a182e5c633ab5207a6be41701f05f7
jainor <jainorcardenas@gmail.com>
2022-01-24 09:08:36 -0500

    feat: add role decorator

---
+++ src/auth/decorators/rol-guard.decorator.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly _reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles: string[] = this._reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    );
+
+    if (!roles) {
+      return true;
+    }
+
+    const gqlCtx = GqlExecutionContext.create(context);
+    const user = gqlCtx.getContext().req.user;
+
+    console.log('usercio', user);
+
+    return user && roles.includes(user.role);
+  }
+}
